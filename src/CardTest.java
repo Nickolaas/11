18444d85b54bee0edf0e71d866110cbfe13d1ced
SirHobo <SirHobo@users.noreply.github.com>
2014-09-15 19:01:05 +0800

    denny lives

--- src/CardTest.java
+++ src/CardTest.java
@@ -10,19 +10,35 @@ public class CardTest {
 		// TODO Auto-generated method stub
 		// Making our two card objects for testing
 		Card bill = new Card("10", "Hearts", 10);
-		Card brad = new Card("10", "Spades", 10);
+		Card mark = new Card("10", "Spades", 10);
+		Card denny = new Card("10", "Spades", 10);
 		
 		// Checking for if the cards are equal
 		// In the if statement it refers to instance brad's method equals to output a boolean
 		// Note: this would also work if it were bill.equals(brad)
-		if (brad.equals(bill)){
-			System.out.println("These cards are the same");
-			System.out.println("They are both "+brad);
+		if (mark.equals(bill)){
+			if (mark.equals(denny)){
+			System.out.println("All these cards are the same");
+			System.out.println("They are all "+mark);
+			}
+			else {
+			System.out.println("Two of the cards are the same, whereas one is not");
+			System.out.println("Two of them are "+mark+" Whereas the other one is "+denny);
+			}
+		}
+		if (mark.equals(denny)){
+			System.out.println("Two of the cards are the same, whereas one is not");
+			System.out.println("Two of them are "+mark+" Whereas the other one is "+bill);
+		}
+		if (bill.equals(denny)){
+			System.out.println("Two of the cards are the same, whereas one is not");
+			System.out.println("Two of them are "+bill+" Whereas the other one is "+mark);
 		}
 		else {
 			System.out.println("These cards are not the same");
-			System.out.println("One is "+brad+" Whereas the other is "+bill);
+			System.out.println("One is "+mark+", the other is "+bill" and the last one is "+denny);
 		}
+
 	}
 
 }
